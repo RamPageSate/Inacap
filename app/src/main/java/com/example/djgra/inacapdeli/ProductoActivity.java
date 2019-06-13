3f88d92670229bd09438fdec10a9d18cb179001d
Daniel <daniel.grados@inacapmail.cl>
2019-06-12 20:55:13 -0400

    prueba223222222

--- app/src/main/java/com/example/djgra/inacapdeli/ProductoActivity.java
+++ app/src/main/java/com/example/djgra/inacapdeli/ProductoActivity.java
@@ -1,10 +1,12 @@
 package com.example.djgra.inacapdeli;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,9 +21,15 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Response;
 import com.example.djgra.inacapdeli.Clases.Categoria;
 import com.example.djgra.inacapdeli.Clases.Producto;
+import com.example.djgra.inacapdeli.Funciones.BddCategoria;
+import com.example.djgra.inacapdeli.Funciones.BddProductos;
 import com.example.djgra.inacapdeli.Funciones.Functions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -36,7 +44,7 @@ public class ProductoActivity extends AppCompatActivity {
     private Date fecha;
     public static ArrayList<Producto> lstProductos = new ArrayList<>();
     public static ArrayList<Categoria> lstCategoria = new ArrayList<>();
-
+    AdapterProductos adapterProductos;
 
 
     @Override
@@ -56,7 +64,7 @@ public class ProductoActivity extends AppCompatActivity {
         btnSalir = findViewById(R.id.btnSalirLstProducto);
         spCategorias = findViewById(R.id.spCategoriaLstProducto);
         lstvProductos = findViewById(R.id.lstvProductos);
-        final AdapterProductos adapterProductos = new AdapterProductos(this);
+        adapterProductos = new AdapterProductos(this,lstProductos);
         ArrayAdapter<Categoria> adapterCategoria = new ArrayAdapter<Categoria>(this,android.R.layout.simple_list_item_1,lstCategoria);
         lstvProductos.setAdapter(adapterProductos);
         spCategorias.setAdapter(adapterCategoria);
@@ -113,28 +121,28 @@ public class ProductoActivity extends AppCompatActivity {
     class AdapterProductos extends ArrayAdapter<Producto> {
         Activity context;
 
-        public AdapterProductos(Activity context) {
-            super(context, R.layout.listviewproductos, lstProductos);
+        public AdapterProductos(Activity context,final ArrayList<Producto> lst) {
+            super(context, R.layout.listviewproductos, lst);
             this.context = context;
         }
 
-        public View getView(final int posicion, View view, ViewGroup parent) {
+        public View getView(final int posicion, View view, ViewGroup parent, ArrayList<Producto> lst) {
 
             LayoutInflater inflater = context.getLayoutInflater();
             View item = inflater.inflate(listviewproductos, null);
-            if (!lstProductos.isEmpty()) {
+            if (!lst.isEmpty()) {
                 ImageView imagen = item.findViewById(R.id.imgLstVendedor);
                 imagen.setId(300+posicion);
                 TextView nombre = item.findViewById(R.id.tvNombreLstProducto);
-                nombre.setText(""+lstProductos.get(posicion).getNombre());
+                nombre.setText(""+lst.get(posicion).getNombre());
                 final TextView precio = item.findViewById(R.id.tvPrecioLstProducto);
-                precio.setText("$ " + lstProductos.get(posicion).getPrecio());
+                precio.setText("$ " + lst.get(posicion).getPrecio());
                 ImageButton btnEdit = item.findViewById(R.id.btnEditProducto);
                 TextView descripcion = item.findViewById(R.id.tvDescripcionListViewProducto);
-                descripcion.setText(""+ lstProductos.get(posicion).getDescripcion());
+                descripcion.setText(""+ lst.get(posicion).getDescripcion());
                 Switch swEstado = item.findViewById(R.id.swEstadoListViewProducto);
                 swEstado.setId(300+posicion);
-                if(lstProductos.get(posicion).getEstado() == 1){
+                if(lst.get(posicion).getEstado() == 1){
                     swEstado.setChecked(true);
                 }else{
                     swEstado.setChecked(false);
@@ -162,6 +170,56 @@ public class ProductoActivity extends AppCompatActivity {
 
             return item;
         }
+    }
+
+
+    public void actualizarListView(){
+        lstProductos.removeAll(lstProductos);
+        final ProgressDialog progressDialog = Functions.CargarDatos("Cangando Productos", ProductoActivity.this);
+        BddProductos.getProducto(ProductoActivity.this, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (!response.toString().equals("[]")) {
+                    for (int x = 0; x < response.length(); x++) {
+                        try {
+                            Log.d("TAG_", "cargara los productos");
+                            final Producto producto = new Producto();
+                            producto.setCodigo(response.getJSONObject(x).getInt("producto_id"));
+                            producto.setNombre(response.getJSONObject(x).getString("producto_nombre"));
+                            producto.setFoto(response.getJSONObject(x).getString("producto_foto"));
+                            producto.setDescripcion(response.getJSONObject(x).getString("producto_descripcion"));
+                            producto.setSku(response.getJSONObject(x).getString("producto_sku"));
+                            producto.setPrecio(response.getJSONObject(x).getInt("producto_precio"));
+                            producto.setStock(response.getJSONObject(x).getInt("producto_stock"));
+                            producto.setEstado(response.getJSONObject(x).getInt("producto_estado"));
+                            producto.setId_fabricante(response.getJSONObject(x).getInt("id_fabricante"));
+                            producto.setId_tipo(response.getJSONObject(x).getInt("id_tipo"));
+                            BddCategoria.getCategoriaByProducto(producto.getCodigo(), ProductoActivity.this, new Response.Listener<JSONArray>() {
+                                @Override
+                                public void onResponse(JSONArray response) {
+                                    if (!response.toString().equals("[]")) {
+                                        ArrayList<Categoria> categoriasProducto = new ArrayList<>();
+                                        for (int x = 0; x < response.length(); x++) {
+                                            try {//falta agrgar las categorias asignadas al producto
+                                                Categoria categoria = new Categoria(response.getJSONObject(x).getInt("categoria_id"), response.getJSONObject(x).getInt("categoria_estado"), response.getJSONObject(x).getString("categoria_nombre"));
+                                                categoriasProducto.add(categoria);
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                        producto.setLstCategoriasProducto(categoriasProducto);
+                                    }
+                                }
+                            }, Functions.FalloInternet(ProductoActivity.this, progressDialog, "No pudo Cargar"));
+                            lstProductos.add(producto);
+                            lstvProductos.setAdapter(adapterProductos);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        },Functions.FalloInternet(ProductoActivity.this,progressDialog,"no pudo Cargar"));
     }
 
 }
