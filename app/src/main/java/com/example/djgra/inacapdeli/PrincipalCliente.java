d4f968b136a7e2f0d96bafed1eec0c5447f3e40c
djgra <daniel.grados@inacapmail.cl>
2019-06-15 11:11:42 -0400

    xD

--- app/src/main/java/com/example/djgra/inacapdeli/PrincipalCliente.java
+++ app/src/main/java/com/example/djgra/inacapdeli/PrincipalCliente.java
@@ -1,26 +1,58 @@
 package com.example.djgra.inacapdeli;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.android.volley.Response;
 import com.example.djgra.inacapdeli.Clases.Persona;
+import com.example.djgra.inacapdeli.Clases.Sede;
+import com.example.djgra.inacapdeli.Funciones.BddSede;
 import com.example.djgra.inacapdeli.Funciones.Functions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class PrincipalCliente extends AppCompatActivity {
     ImageView fotoUsr;
+    TextView tvSedeActual, tvSaldoActual;
     public static Persona cliente = new Persona();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal_cliente);
-
         fotoUsr= (ImageView) findViewById(R.id.imgFotoCliente);
-
+        tvSedeActual = (TextView) findViewById(R.id.tvSedeActualCliente);
+        tvSaldoActual = (TextView) findViewById(R.id.tvSaldoCliente);
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             cliente = (Persona) bundle.getSerializable("usr");
+            tvSaldoActual.setText(tvSaldoActual.getText()+""+cliente.getSaldo());
             fotoUsr.setImageBitmap(Functions.StringToBitMap(cliente.getFoto()));
         }
+        final ProgressDialog progressDialog = Functions.CargarDatos("Cargando...",this);
+        BddSede.getSede(PrincipalCliente.this, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (!response.toString().equals("[]")) {
+                    for (int x = 0; x < response.length(); ++x) {
+                        try {
+                            final Sede sede = (new Sede(response.getJSONObject(x).getInt("sede_id"),
+                                    response.getJSONObject(x).getInt("sede_estado"),
+                                    response.getJSONObject(x).getString("sede_direccion")));
+                            if(sede.getCodigo()  == cliente.getSede()){
+                                tvSedeActual.setText(sede.getDireccion().toUpperCase());
+                                progressDialog.dismiss();
+                                break;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }, Functions.FalloInternet(PrincipalCliente.this,progressDialog,"No Pudo Cargar"));
     }
 }
