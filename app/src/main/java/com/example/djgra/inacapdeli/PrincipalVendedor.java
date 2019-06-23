91a4540654bbaf197422023935c6d81623b674a5
djgra <daniel.grados@inacapmail.cl>
2019-06-23 19:59:03 -0400

    xD

---
+++ app/src/main/java/com/example/djgra/inacapdeli/PrincipalVendedor.java
@@ -0,0 +1,15 @@
+package com.example.djgra.inacapdeli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrincipalVendedor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_vendedor);
+
+
+    }
+}
