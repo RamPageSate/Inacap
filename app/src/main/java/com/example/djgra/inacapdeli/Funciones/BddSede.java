de6be4a081ac9dc3ddc250e25fe44cce81170a8f
RampageState <rampagestate@gmail.com>
2019-06-26 19:56:33 -0400

    EstoEsUnaPrueba
    Signed-off-by: RampageState <rampagestate@gmail.com>

--- app/src/main/java/com/example/djgra/inacapdeli/Funciones/BddSede.java
+++ app/src/main/java/com/example/djgra/inacapdeli/Funciones/BddSede.java
@@ -17,54 +17,85 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BddSede {
-    private static Map<String,String> map = new HashMap<>();
-    private static  String urlBase = "https://laxjbz6j-site.gtempurl.com/igniter/funcion/";
+    private static Map<String, String> map = new HashMap<>();
+    private static String urlBase = "https://laxjbz6j-site.gtempurl.com/igniter/funcion/";
 
-    /*
-     *
-     * */
-    public static void setSede(final Sede sede, Context context, Response.Listener<String> listener, Response.ErrorListener errorListener){
+    /**
+     * @param sede          Sede que se desea insertar.
+     * @param context       Contexto de la actividad
+     * @param errorListener Accion en caso de error que se desea al capturar  la respuesta
+     * @param listener      Accion que se desea al capturar la respuesta.
+     * @return void
+     * Metodo para insertar una sede
+     */
+    public static void setSede(final Sede sede, Context context, Response.Listener<String> listener, Response.ErrorListener errorListener) {
         RequestQueue requestQueue = Volley.newRequestQueue(context);
-        StringRequest stringRequest = new StringRequest(Request.Method.POST,urlBase+"setSede",listener,errorListener){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlBase + "setSede", listener, errorListener) {
             @Override
-            protected Map<String,String> getParams() throws AuthFailureError {
+            protected Map<String, String> getParams() throws AuthFailureError {
                 map.clear();
-                map= new HashMap<String,String>();
-                map.put("sede_direccion",sede.getDireccion().toUpperCase());
-                map.put("sede_estado",String.valueOf(sede.getEstado()));
+                map = new HashMap<String, String>();
+                map.put("sede_direccion", sede.getDireccion().toUpperCase());
+                map.put("sede_estado", String.valueOf(sede.getEstado()));
                 return map;
             }
         };
         requestQueue.add(stringRequest);
     }
-    public static void getSede(Context context, Response.Listener<JSONArray> listener, Response.ErrorListener errorListener){
+
+    /**
+     * @param context       Contexto de la actividad
+     * @param errorListener Accion en caso de error que se desea al capturar  la respuesta
+     * @param listener      Accion que se desea al capturar la respuesta.
+     * @return void.
+     * Metodo para traer todas las sedes.
+     */
+    public static void getSede(Context context, Response.Listener<JSONArray> listener, Response.ErrorListener errorListener) {
         RequestQueue requestQueue = Volley.newRequestQueue(context);
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,urlBase+"getSede",null,listener,errorListener);
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, urlBase + "getSede", null, listener, errorListener);
         requestQueue.add(request);
     }
-    public static void updateSede(final Sede sede, Context context, Response.Listener<String> listener, Response.ErrorListener errorListener){
+
+    /**
+     * @param sede          Sede para ser modificadas en el web service
+     * @param context       Contexto de la actividad
+     * @param errorListener Accion en caso de error que se desea al capturar  la respuesta
+     * @param listener      Accion que se desea al capturar la respuesta.
+     * @return void.
+     * Metodo para modificar una sede en el web service.
+     */
+    public static void updateSede(final Sede sede, Context context, Response.Listener<String> listener, Response.ErrorListener errorListener) {
         RequestQueue requestQueue = Volley.newRequestQueue(context);
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlBase+"updateSede",listener,errorListener){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlBase + "updateSede", listener, errorListener) {
             @Override
-            protected Map<String,String> getParams() throws AuthFailureError{
+            protected Map<String, String> getParams() throws AuthFailureError {
                 map.clear();
-                map=new HashMap<String, String>();
-                map.put("sede_id",String.valueOf(sede.getCodigo()));
-                map.put("sede_direccion",sede.getDireccion().toUpperCase());
-                map.put("sede_estado",String.valueOf(sede.getEstado()));
+                map = new HashMap<String, String>();
+                map.put("sede_id", String.valueOf(sede.getCodigo()));
+                map.put("sede_direccion", sede.getDireccion().toUpperCase());
+                map.put("sede_estado", String.valueOf(sede.getEstado()));
                 return map;
             }
         };
         requestQueue.add(stringRequest);
     }
-    public static void deleteSede(final Sede sede, Context context, Response.Listener<String> listener, Response.ErrorListener errorListener){
+
+    /**
+     * @param sede          Sede para ser eliminada en el web service
+     * @param context       Contexto de la actividad
+     * @param errorListener Accion en caso de error que se desea al capturar  la respuesta
+     * @param listener      Accion que se desea al capturar la respuesta.
+     * @return void.
+     * Metodo para eliminar una sede en el web service.
+     */
+    public static void deleteSede(final Sede sede, Context context, Response.Listener<String> listener, Response.ErrorListener errorListener) {
         RequestQueue requestQueue = Volley.newRequestQueue(context);
-        StringRequest stringRequest = new StringRequest(Request.Method.POST,urlBase+"deleteSede",listener,errorListener){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlBase + "deleteSede", listener, errorListener) {
             @Override
-            protected  Map<String, String> getParams() throws  AuthFailureError{
+            protected Map<String, String> getParams() throws AuthFailureError {
                 map.clear();
-                map = new HashMap<String,String>();
-                map.put("sede_id",String.valueOf(sede.getCodigo()));
+                map = new HashMap<String, String>();
+                map.put("sede_id", String.valueOf(sede.getCodigo()));
                 return map;
             }
         };
--- app/src/main/java/com/example/djgra/inacapdeli/Funciones/BddTipo.java
+++ app/src/main/java/com/example/djgra/inacapdeli/Funciones/BddTipo.java
@@ -18,24 +18,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BddTipo {
-    private static  String urlBase = "https://laxjbz6j-site.gtempurl.com/igniter/funcion/";
+    private static String urlBase = "https://laxjbz6j-site.gtempurl.com/igniter/funcion/";
     public static ArrayList<Tipo> lstTipo = new ArrayList<>();
-    private static Map<String,String> map = new HashMap<>();
+    private static Map<String, String> map = new HashMap<>();
 
-    public static void getTipo(Context context, Response.Listener<JSONArray> listener, Response.ErrorListener errorListener){
+    /**
+     * @param context       Contexto de la actividad
+     * @param errorListener Accion en caso de error que se desea al capturar  la respuesta
+     * @param listener      Accion que se desea al capturar la respuesta.
+     * @return void
+     * Metodo que trae todos los tipos del web service.
+     */
+    public static void getTipo(Context context, Response.Listener<JSONArray> listener, Response.ErrorListener errorListener) {
         RequestQueue requestQueue = Volley.newRequestQueue(context);
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,urlBase+"getTipo",null,listener,errorListener);
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, urlBase + "getTipo", null, listener, errorListener);
         requestQueue.add(request);
     }
 
-    public static void setTipo(final Tipo tipo, Context context, Response.Listener<String> listener, Response.ErrorListener errorListener){
+    public static void setTipo(final Tipo tipo, Context context, Response.Listener<String> listener, Response.ErrorListener errorListener) {
         RequestQueue requestQueue = Volley.newRequestQueue(context);
-        StringRequest stringRequest = new StringRequest(Request.Method.POST,urlBase+"setTipo",listener,errorListener){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlBase + "setTipo", listener, errorListener) {
             @Override
-            protected Map<String,String> getParams() throws AuthFailureError {
+            protected Map<String, String> getParams() throws AuthFailureError {
                 map.clear();
-                map= new HashMap<String,String>();
-                map.put("tipo",tipo.getNombre().toUpperCase());
+                map = new HashMap<String, String>();
+                map.put("tipo", tipo.getNombre().toUpperCase());
                 return map;
             }
         };
@@ -62,9 +69,6 @@ public class BddTipo {
         };
         requestQueue.add(stringRequest);
     }
-
-
-
 
 
 }
